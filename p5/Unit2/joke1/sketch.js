b707f6d41bd9eb6665f4e98b2cc6f13006f7d47b
dontef0004 <dpfost1@ilstu.edu>
2020-09-29 04:12:16 -0500

    .

--- p5/Unit2/joke1/sketch.js
+++ p5/Unit2/joke1/sketch.js
@@ -1,7 +1,29 @@
+let state = 0;
+
 function setup() {
   createCanvas(500, 500);
+  textAlign(CENTER);
+  textSize(24);
 }
 
 function draw() {
 
+  switch (state){
+
+    case 0:
+    background('purple');
+    text('why did the baby cross the road?', 250, 250);
+    break;
+
+    case 1:
+    background('orange')
+    text('because it was stapled to the chicken.', 250, 250);
+    break;
+  }
+
+}
+function mouseReleased() {
+  state++;
+
+  if(state > 2) state = 0;
 }
--- p5/Unit2/joke2/sketch.js
+++ p5/Unit2/joke2/sketch.js
@@ -1,20 +1,29 @@
-
-
+let state = 0;
 
 function setup() {
   createCanvas(500, 500);
   textAlign(CENTER);
-  rectMode(CENTER);
+  textSize(24);
 }
 
 function draw() {
 
-  switch(state)
+  switch (state){
 
-  case 0:
-  background('red');
-  text("Why did the chicken\n cross the road?", 250, 250);
+    case 0:
+    background('red');
+    text('why did the chicken cross the road?', 250, 250);
+    break;
 
-  timer = timer + 1
+    case 1:
+    background('green')
+    text('because it was having a bad day.', 250, 250);
+    break;
+  }
 
+}
+function mouseReleased() {
+  state++;
+
+  if(state > 2) state = 0;
 }
