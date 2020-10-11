8eef624249110b8086a4a4868e4995e69a73d087
dontef0004 <dpfost1@ilstu.edu>
2020-10-11 15:24:36 -0500

    .

--- p5/Unit2/mp2/sketch.js
+++ p5/Unit2/mp2/sketch.js
@@ -1,20 +1,22 @@
-let myState = 0;
+let state = 0;
 var img1, img2, img3 ;
 var mic;
 var vol;
+let timer = 0;
 
 
 function preload(){
   img1 = loadImage("assets/tshirt.jpg");
   img2 = loadImage("assets/gheart1.jpg");
   img3 = loadImage("assets/noH8onTee.jpg");
+  mic = new p5.AudioIn();
+  mic.start();
+  vol = (mic.getLevel()).toFixed(2);
 }
 function setup() {
   createCanvas(700, 700);
 
-  image(img1, width/2, height/2);
-  image(img2, width/2, height/2);
-  image(img3, width/2, height/2);
+
 
   mic = new p5.AudioIn();
   mic.start();
@@ -24,11 +26,16 @@ function draw() {
 
   vol = (mic.getLevel()).toFixed(2);
 
-  switch(myState) {
+  switch(state) {
+
     case 0:
     background('purple');
-    text('Click Button to Get a Design!', 250, 250);
+    text('Wait to Get a Design!', 250, 250);
     image(img1, width/2, height/2, 230, 200);
+    timer ++;
+      if (timer >100){
+        state = 1;
+      }
     break;
 
     case 1:
@@ -36,8 +43,11 @@ function draw() {
     text('Here is your design. Say something to put it on a shirt!', 200, 235);
     text("My volume is " + vol, 10, 60);
     image(img2, width/2, height/2, 230, 200);
-    if (vol > .010){
-      state = 3;
+    mic = new p5.AudioIn();
+    mic.start();
+    vol = (mic.getLevel()).toFixed(2);
+    if (vol > 0.10){
+      state = 2;
     }
     break;
 
@@ -45,25 +55,15 @@ function draw() {
     background('blue');
     text('Ta Da!', 250, 250);
     image(img3, width/2, height/2, 230, 200);
+    break;
+
+}
+
+
+
+
+  function mouseReleased() {
+    state++ ;
+
+    }
   }
-
-    fill('white');
-    rect(10, 10, 50, 25);
-}
-
-
-
-function mouseReleased() {
-
-
-
-  if (( mouseX > 10) && (mouseX < 60) && (mouseY > 10)  && (mouseY < 35));
-  myState++;
-
-  if (myState > 2) {
-    myState = 0;
-}
-function touchStarted() {
-  getAudioContext().resume();
-}
-}
